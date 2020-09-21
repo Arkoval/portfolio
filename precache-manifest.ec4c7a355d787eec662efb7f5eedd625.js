de764b853f691f358d7783911c38e4ada5aeeb8f
Arkoval <arkkowalski95@gmail.com>
2020-09-21 12:10:07 +0200

    Updates

---
+++ precache-manifest.ec4c7a355d787eec662efb7f5eedd625.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19b65ad88fe445975c07d0ab0efb822",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "901d6c266f146fbae168",
+    "url": "/portfolio/static/css/main.ca122773.chunk.css"
+  },
+  {
+    "revision": "d7380e85c5913f90d894",
+    "url": "/portfolio/static/js/2.f001b678.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/portfolio/static/js/2.f001b678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901d6c266f146fbae168",
+    "url": "/portfolio/static/js/main.bc238fda.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c0efca774fd9cc8b386ce070dd48e405",
+    "url": "/portfolio/static/media/enso-footer.c0efca77.svg"
+  },
+  {
+    "revision": "583d44e86396a2def27099c71b6bf48e",
+    "url": "/portfolio/static/media/enso-hero.583d44e8.svg"
+  },
+  {
+    "revision": "9b33101d6a918566661c51fa3a743586",
+    "url": "/portfolio/static/media/enso.9b33101d.svg"
+  }
+]);
